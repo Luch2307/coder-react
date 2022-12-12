9c93394729569eb62b0a76a593b6070acfd6d542
Luch2307 <lucianoierace@hotmail.com>
2022-12-12 17:37:20 -0300

    react-coder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './componentes/Navbar.jsx'
+function App() {
+  return (
+    <div className="container">
+      <Navbar/>  
+    </div>
+  );
+}
+
+export default App;
